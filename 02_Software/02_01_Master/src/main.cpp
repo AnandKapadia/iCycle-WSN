636533991fb879835063aafd97678f80fb930999
Homer Baker <hfbaker4@gmail.com>
2018-04-04 08:58:08 +0000

    Display bicycle in each direction

--- 02_Software/02_01_Master/src/main.cpp
+++ 02_Software/02_01_Master/src/main.cpp
@@ -8,10 +8,12 @@ const uint16_t SCREEN_HEIGHT = 1160;
 int main() {
 
   // Initialize the SDL objects we want to use
-  SDL_Window   *window     = NULL;
-  SDL_Renderer *renderer   = NULL;
-  SDL_Surface  *image      = NULL;
-  SDL_Texture  *texture    = NULL;
+  SDL_Window   *window          = NULL;
+  SDL_Renderer *renderer        = NULL;
+  SDL_Surface  *vehicle_image   = NULL;
+  SDL_Texture  *vehicle_texture = NULL;
+  SDL_Surface  *bicycle_image   = NULL;
+  SDL_Texture  *bicycle_texture = NULL;
 
   // Initialize SDL
   if( SDL_Init(SDL_INIT_VIDEO) < 0 ) {
@@ -27,35 +29,63 @@ int main() {
     if(window == NULL) {
       printf("Window could not be created! SDL_Error: %s\n", SDL_GetError());
     }
+    
     renderer = SDL_CreateRenderer(window, -1, 0);
     if(renderer == NULL) {
       printf("Renderer could not be created! SDL_Error: %s\n", SDL_GetError());
     }
     SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
     SDL_RenderClear(renderer);
-//    char *dataPath = NULL;
-//    dataPath = strcat(SDL_GetBasePath(), "../data/vehicle.bmp");
-//    printf("PATH: %s\n", dataPath);
-    image = SDL_LoadBMP("/home/pi/Documents/iCycle-WSN/02_Software/02_01_Master/data/vehicle.bmp");
-    if(image == NULL) {
+
+    vehicle_image = SDL_LoadBMP("/home/pi/Documents/iCycle-WSN/02_Software/02_01_Master/data/vehicle.bmp");
+    if(vehicle_image == NULL) {
       printf("Image could not be created! SDL_Error: %s\n", SDL_GetError());
     }
-    texture = SDL_CreateTextureFromSurface(renderer, image);
-    if(texture == NULL) {
+
+    vehicle_texture = SDL_CreateTextureFromSurface(renderer, vehicle_image);
+    if(vehicle_texture == NULL) {
       printf("Texture could not be created! SDL_Error: %s\n", SDL_GetError());
     }
-    SDL_Rect dstrect = {(SCREEN_WIDTH-image->w)/2, (SCREEN_HEIGHT-image->h)/2, image->w, image->h};
-    SDL_RenderCopy(renderer, texture, NULL, &dstrect);
+
+    bicycle_image = SDL_LoadBMP("/home/pi/Documents/iCycle-WSN/02_Software/02_01_Master/data/bicycle.bmp");
+    if(bicycle_image == NULL) {
+      printf("Image could not be created! SDL_Error: %s\n", SDL_GetError());
+    }
+
+    bicycle_texture = SDL_CreateTextureFromSurface(renderer, bicycle_image);
+    if(bicycle_texture == NULL) {
+      printf("Texture could not be created! SDL_Error: %s\n", SDL_GetError());
+    }
+
+    SDL_Rect vehiclePos = {(SCREEN_WIDTH-vehicle_image->w)/2, (SCREEN_HEIGHT-vehicle_image->h)/2,
+                                         vehicle_image->w,                   vehicle_image->h };
+    SDL_Rect bicyclePos0 = {0, 0, (bicycle_image->w)/5, (bicycle_image->h)/5};
+    SDL_Rect bicyclePos1 = {SCREEN_WIDTH/2-((bicycle_image->w)/5)/2, 0, (bicycle_image->w)/5, (bicycle_image->h)/5};
+    SDL_Rect bicyclePos2 = {SCREEN_WIDTH-(bicycle_image->w)/5, 0, (bicycle_image->w)/5, (bicycle_image->h)/5};
+    SDL_Rect bicyclePos3 = {0, SCREEN_HEIGHT/2-((bicycle_image->h)/5)/2, (bicycle_image->w)/5, (bicycle_image->h)/5};
+    SDL_Rect bicyclePos4 = {SCREEN_WIDTH-(bicycle_image->w)/5, SCREEN_HEIGHT/2-((bicycle_image->h)/5)/2, (bicycle_image->w)/5, (bicycle_image->h)/5};
+    SDL_Rect bicyclePos5 = {0, SCREEN_HEIGHT-(bicycle_image->h)/5, (bicycle_image->w)/5, (bicycle_image->h)/5};
+    SDL_Rect bicyclePos6 = {SCREEN_WIDTH/2-((bicycle_image->w)/5)/2, SCREEN_HEIGHT-(bicycle_image->h)/5, (bicycle_image->w)/5, (bicycle_image->h)/5};
+    SDL_Rect bicyclePos7 = {SCREEN_WIDTH-(bicycle_image->w)/5, SCREEN_HEIGHT-(bicycle_image->h)/5, (bicycle_image->w)/5, (bicycle_image->h)/5};
+    SDL_RenderCopy(renderer, vehicle_texture, NULL, &vehiclePos);
+    SDL_RenderCopy(renderer, bicycle_texture, NULL, &bicyclePos0);
+    SDL_RenderCopy(renderer, bicycle_texture, NULL, &bicyclePos1);
+    SDL_RenderCopy(renderer, bicycle_texture, NULL, &bicyclePos2);
+    SDL_RenderCopy(renderer, bicycle_texture, NULL, &bicyclePos3);
+    SDL_RenderCopy(renderer, bicycle_texture, NULL, &bicyclePos4);
+    SDL_RenderCopy(renderer, bicycle_texture, NULL, &bicyclePos5);
+    SDL_RenderCopy(renderer, bicycle_texture, NULL, &bicyclePos6);
+    SDL_RenderCopy(renderer, bicycle_texture, NULL, &bicyclePos7);
     SDL_RenderPresent(renderer);
 
     // Wait two seconds
-    SDL_Delay( 10000 );
+    SDL_Delay(10000);
   }
 
   // Destroy the SDL objects
   SDL_DestroyWindow(window);
-  SDL_DestroyTexture(texture);
-  SDL_FreeSurface(image);
+  SDL_DestroyTexture(vehicle_texture);
+  SDL_FreeSurface(vehicle_image);
   SDL_DestroyRenderer(renderer);
 
   // Quit SDL subsystems
