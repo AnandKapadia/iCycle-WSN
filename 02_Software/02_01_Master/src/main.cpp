198831e5c3f168a5e6aae4d014ff3f583c746a04
Homer Baker <hfbaker@andrew.cmu.edu>
2018-04-04 03:32:02 -0400

    Added feature to display vehicle

--- 02_Software/02_01_Master/src/main.cpp
+++ 02_Software/02_01_Master/src/main.cpp
@@ -8,13 +8,13 @@ const int SCREEN_HEIGHT = 480;
 
 int main() {
 	//The window we'll be rendering to
-	SDL_Window* window = NULL;
+	SDL_Window *window = NULL;
 	
 	//The surface contained by the window
-	SDL_Surface* screenSurface = NULL;
+	SDL_Surface *screenSurface = NULL;
 
 	//Initialize SDL
-	if( SDL_Init( SDL_INIT_VIDEO ) < 0 ) {
+	if( SDL_Init(SDL_INIT_VIDEO) < 0 ) {
 		printf( "SDL could not initialize! SDL_Error: %s\n", SDL_GetError() );
 	}
 	else {
@@ -28,17 +28,19 @@ int main() {
 			printf( "Window could not be created! SDL_Error: %s\n", SDL_GetError() );
 		}
 		else {
-			//Get window surface
-			screenSurface = SDL_GetWindowSurface( window );
-
-			//Fill the surface white
-			SDL_FillRect( screenSurface, NULL, SDL_MapRGB( screenSurface->format, 0xFF, 0xFF, 0xFF ) );
-			
-			//Update the surface
-			SDL_UpdateWindowSurface( window );
+		
+      SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, 0);
+      SDL_Surface *image = SDL_LoadBMP("vehicle.bmp");
+      SDL_Texture *texture = SDL_CreateTextureFromSurface(renderer, image);
+      SDL_RenderCopy(renderer, texture, NULL, NULL);
+      SDL_RenderPresent(renderer);
 
 			//Wait two seconds
 			SDL_Delay( 2000 );
+
+      SDL_DestroyTexture(texture);
+      SDL_FreeSurface(image);
+      SDL_DestroyRenderer(renderer);
 		}
 	}
 
